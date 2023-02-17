96d3055f7de0e39b6fb625494674ea9639388110
Kevin Dunn <kdunn@hex.tech>
2023-02-17 10:42:11 -0800

    Fixing the query.

--- claires-cool-query.sql
+++ claires-cool-query.sql
@@ -3,7 +3,7 @@ select *, current_timestamp()
 from analytics.the_good_place.
 
 where kevin = 'cool'
-    or sarah = 'great'
-    or john = 'awesome'
-    or sara = 'amazing'
+    or sarah != 'great'
+    or john != 'awesome'
+    or sara != 'amazing'
     
